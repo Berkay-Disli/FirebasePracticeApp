7d6e28978a896f0ebe58d313db87e76fc9b08754
Berkay-Disli <berkaydisli.work@gmail.com>
2023-03-21 15:45:26 +0300

    Initial Commit

---
+++ FirebasePracticeApp/FirebasePracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePracticeAppApp.swift
+//  FirebasePracticeApp
+//
+//  Created by Berkay Disli on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
